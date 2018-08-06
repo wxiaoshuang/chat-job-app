7ce4611ae0d8d7530b611751ae500f47500d8ca1
wxiaoshuang <2177367028@qq.com>
2018-08-06 22:41:59 +0800

    basic react knowledgee

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## 技术栈
 webpack+ react+redux+react-router+nodejs+mongodb
 ## 项目开发过程
-### chapter02
 1. 全局安装脚手架 `npm i -g create-react-app`,安装完毕后，`create-react-app chat`建立一个chat的项目，安装完毕之后进入`cd chat && npm run dev`启动开发项目， `npm run eject`弹出自定义配置
 2. es6基础知识
 3. *express + mongodb*
+4. react基础知识
--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+// 组件之间用props传递数据
+class App extends  Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: ['javascript', 'css', 'html']
+    } ;
+  }
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+  addSubject() {
+    // 不能修改状态，而是返回新的状态
+      this.setState({
+        subject: [...this.state.subject, 'hello'+Date.now()]
+      })
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const name = 'hello';
+    console.log('render');
+    return (<div>
+      {/* 事件绑定，this引用的问题 */}
+      <button onClick={()=>{this.addSubject()}}>添加学科</button>
+      这是aPP,{name}
+      <Child className={name}></Child>
+      {this.state.subject.map(v => {
+        return (<li key={v}>{v}</li>);
+      })}
+      </div>)
   }
 }
-
+class Child extends Component {
+  componentWillMount() {
+    console.log('child-componentWillMount');
+  }
+  componentDidMount() {
+    console.log('child-componentDidMount');
+  }
+  render() {
+    console.log('child-render');
+    return <div>{this.props.className}</div>
+  }
+}
 export default App;
