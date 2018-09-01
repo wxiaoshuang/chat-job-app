ba745bec04a54c483dca37ef6866b64b0b1832c8
wxiaoshuang <2177367028@qq.com>
2018-09-01 16:29:53 +0800

    install body-parser and cookie parse to solve the post request and handle cookie

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./user');
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+app.use(cookieParser())
+app.use(bodyParser.json())
 app.use('/user', userRouter);
 // nodemon监控node程序，自动重启
 app.listen(9093, function () {
--- server/user.js
+++ server/user.js
@@ -1,7 +1,18 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const models = require('./model')
+const User = models.getModel('User')
+// 获取用户列表
+router.get('/list',function(req,res) {
+    User.find({},function(err, doc) {
+        return res.json(doc)
+    })
+})
+router.post('/register', function(req, res){
+    console.log(req.body.data);
+})
 router.get('/info', function(req, res){
     return res.json({code:1});
 })
 router.post('/register')
-module.exports = router;
+module.exports = router
